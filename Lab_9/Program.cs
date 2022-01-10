daf07e07836b0598e3a46df02beff9dab2b11294
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-11 00:48:18 +0300

    Добавьте файлы проекта.

---
+++ Lab_9/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int cmd=0;
+            try
+            {
+                Console.WriteLine("Вас приветствует калькулятор!");
+                Console.Write("Введите целое число A =");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите целое число B =");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите код операции: \n 1 - сложение\n 2 - вычитание\n 3 - умножение\n 4 - частное\n");
+                Console.WriteLine("Ваш выбор: ");
+                cmd = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка ввода" + ex.Message);
+                Console.ReadKey();
+                return;
+             }
+            switch (cmd)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Результат равен = {0}", a + b);
+                        break;
+                    }
+                    case 2:
+
+                    {
+                        Console.WriteLine("Результат равен = {0}", a - b);
+                        break;
+                    }
+                        case 3:
+                    {
+                        Console.WriteLine("Результат равен = {0}", a*b);
+                        break;
+                    }
+                case 4:
+                    {
+                        try
+                        {
+                            Console.WriteLine("Результат равен = {0:f2}", a / b);
+                        }
+                        catch(Exception ex) 
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    }
+
+                    default:
+                    {
+                        Console.WriteLine("Нет операции с указанным номером!");
+                        break;
+                    }
+
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
